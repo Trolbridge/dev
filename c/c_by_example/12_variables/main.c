fb7dadd3d481379d0e1dec4714cf254274ea7794
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-09-24 23:13:39 -0400

    c

---
+++ c/c_by_example/12_variables/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Variable declarations
+// constants
+// Eg const int passsing_marks = 40;
+
+// hi
+
+int main() {
+    printf("Hi there!\n");
+}
---
+++ c/c_by_example/my_base/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// Variable declarations
+// constants
+// Eg const int passsing_marks = 40;
+
+int main() {
+    printf("Hi there!\n");
+}
