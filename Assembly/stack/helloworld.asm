3abee5a7e3bb6869129ba1c41ae0eac31471a422
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-10-01 15:12:00 -0400

    Update asm with a jump, pop and a compare

--- Assembly/stack/helloworld.asm
+++ Assembly/stack/helloworld.asm
@@ -10,15 +10,36 @@ global _main
 
  
 _main:
- 
-;    mov     rdx, 13          ; number of bytes to write - one for each letter plus LF char.
-    mov     rdx,len             ;third argument: message length
-    lea     rsi, [rel msg1]   ; move the memory address of our message string into rsi
+
+    mov     rcx, 4                  ; Counter 9
+L1:
+    push    rcx
+    mov     rdx, newLineLen             ;third argument: message length
+    lea     rsi, [rel newLine]   ; move the memory address of our message string into rsi
     mov     rdi, 1           ; write to the STDOUT file
     mov     rax, 0x2000004   ; invoke SYS_WRITE (kernel opcode 4)
     syscall
-    mov     rdx, 13          ; number of bytes to write - one for each letter plus LF char.
+    pop     rcx
+
+    dec     rcx
+    cmp     rcx, 0x0                  
+    jnz     L1
+
+    mov     rdx, newLineLen             ;third argument: message length
+    lea     rsi, [rel newLine]   ; move the memory address of our message string into rsi
+    mov     rdi, 1           ; write to the STDOUT file
+    mov     rax, 0x2000004   ; invoke SYS_WRITE (kernel opcode 4)
+    syscall
+
+    mov     rdx, helloLen             ;third argument: message length
     lea     rsi, [rel msg]   ; move the memory address of our message string into rsi
+    mov     rdi, 1           ; write to the STDOUT file
+    mov     rax, 0x2000004   ; invoke SYS_WRITE (kernel opcode 4)
+    syscall
+
+;    mov     rdx, 13          ; number of bytes to write - one for each letter plus LF char.
+    mov     rdx, jesusLen          ; number of bytes to write - one for each letter plus LF char.
+    lea     rsi, [rel msg1]   ; move the memory address of our message string into rsi
     mov     rdi, 1           ; write to the STDOUT file
     mov     rax, 0x2000004   ; invoke SYS_WRITE (kernel opcode 4)
     syscall
@@ -28,7 +49,12 @@ _main:
     syscall
 
 SECTION .data
+newLine     db      '\n!', 0x0A ; assign msg variable
+newLineLen equ     $ - newLine             ;length of our dear string
+
 msg     db      'Hello World!', 0x0A ; assign msg variable
+helloLen equ     $ - msg             ;length of our dear string
+
 msg1    db      'Hello Jesus!', 0x0A
-len equ     $ - msg             ;length of our dear string
+jesusLen equ     $ - msg1             ;length of our dear string
 
